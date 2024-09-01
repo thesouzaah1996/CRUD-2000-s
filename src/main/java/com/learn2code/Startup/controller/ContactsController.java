6a152a46c4088fe6c0f2c70de0c071dd68f05823
thesouzaah <matheus.de.souza.code@gmail.com>
2024-09-01 19:45:23 -0300

    add function add at the system.

--- src/main/java/com/learn2code/Startup/controller/ContactsController.java
+++ src/main/java/com/learn2code/Startup/controller/ContactsController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,5 +22,21 @@ public class ContactsController {
         List<Contacts> contacts = contactsService.findAllContacts();
         model.addAttribute("contacts", contacts);
         return "contacts";
+    }
+
+    @PostMapping("/contacts")
+    public String createContact(@RequestParam("name") String name,
+                                @RequestParam("email") String email,
+                                @RequestParam("phone") String phone,
+                                @RequestParam("title") String title,
+                                Model model) {
+        try {
+            contactsService.createContact(name, email, phone, title);
+            model.addAttribute("successMessage", "Contact created successfully!");
+            return "redirect:/contacts";
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "contacts";
+        }
     }
 }
--- src/main/java/com/learn2code/Startup/service/ContactsService.java
+++ src/main/java/com/learn2code/Startup/service/ContactsService.java
@@ -2,9 +2,11 @@ package com.learn2code.Startup.service;
 
 import com.learn2code.Startup.model.Contacts;
 import com.learn2code.Startup.repository.ContactsRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,5 +17,18 @@ public class ContactsService {
 
     public List<Contacts> findAllContacts() {
         return contactsRepository.findAll();
+    }
+
+    @Transactional
+    public Contacts createContact(String name, String email, String phone, String title) {
+
+        Contacts newContact = new Contacts();
+        newContact.setName(name);
+        newContact.setEmail(email);
+        newContact.setPhone(phone);
+        newContact.setTitle(title);
+        newContact.setCreated(LocalDateTime.now());
+
+        return contactsRepository.save(newContact);
     }
 }
